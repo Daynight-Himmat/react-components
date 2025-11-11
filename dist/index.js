01fbcc7368fe5b6ab791f04207f49b6b6f7e120f
Himmat Suthar <64137801+Daynight-Himmat@users.noreply.github.com>
2025-11-11 18:10:30 +0530

    update Dist

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TextInputController = exports.DropdownController = exports.TextInput = exports.Dropdown = exports.AppText = void 0;
-var AppText_1 = require("./components/text/AppText");
-Object.defineProperty(exports, "AppText", { enumerable: true, get: function () { return __importDefault(AppText_1).default; } });
+exports.DropdownController = exports.Dropdown = void 0;
 var Dropdown_1 = require("./components/dropdown/Dropdown");
 Object.defineProperty(exports, "Dropdown", { enumerable: true, get: function () { return __importDefault(Dropdown_1).default; } });
-var AppTextInput_1 = require("./components/textInput/AppTextInput");
-Object.defineProperty(exports, "TextInput", { enumerable: true, get: function () { return __importDefault(AppTextInput_1).default; } });
 var DropdownController_1 = require("./controller/dropDownController/DropdownController");
 Object.defineProperty(exports, "DropdownController", { enumerable: true, get: function () { return __importDefault(DropdownController_1).default; } });
-var AppTextInputController_1 = require("./controller/textInputController/AppTextInputController");
-Object.defineProperty(exports, "TextInputController", { enumerable: true, get: function () { return __importDefault(AppTextInputController_1).default; } });
