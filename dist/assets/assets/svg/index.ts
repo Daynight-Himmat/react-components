9ad4ea39282a176948b34fd7ce895f1a5b7bf390
Himmat Suthar <64137801+Daynight-Himmat@users.noreply.github.com>
2026-09-25 12:45:56 +0530

    uipdate

---
+++ dist/assets/assets/svg/index.ts
@@ -0,0 +1,5 @@
+export {default as ArrowUp } from './ArrowUp';
+export {default as CheckIcon} from './CheckIcon';
+export {default as ArrowDown } from './ArrowDown';
+export {default as ClearIcon } from './ClearIcon';
+export {default as SearchIcon } from './SearchIcon';
